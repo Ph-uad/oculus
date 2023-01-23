d126655ff5f5ced512a40d17e430a9db3312dc93
Ph-uad <temidayo26j@gmail.com>
2023-01-23 03:09:33 +0100

    swiper implentation

--- layout/Gallery.vue
+++ layout/Gallery.vue
@@ -2,58 +2,85 @@
   <section class="section">
     <div class="gallery__container">
       <h2 class="heading--secondary title--margin">Gallery</h2>
-      <div class="slider">
-            <div class="grid">
-        <picture class="picture"><img src="https://images.unsplash.com/photo-1617373310678-e6f25a2d452a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80" alt="" class="img" /></picture>
-        <picture class="picture"><img src="https://images.unsplash.com/photo-1604785846291-2cd9192b1bef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2336&q=80" alt="" class="img" /></picture>
-        <picture class="picture"><img src="https://images.unsplash.com/photo-1550607326-2df38662b7dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="" class="img" /></picture>
-      </div>
-      </div>
-
-    </div>
+          <swiper
+            :spaceBetween="3"
+            :centeredSlides="true"
+            :autoplay="{
+              delay: 3000,
+              disableOnInteraction: false,
+            }"
+            :modules="modules"
+            class="swiper"
+          >
+            <swiper-slide>
+              <picture class="picture"
+                ><img
+                  src="https://images.unsplash.com/photo-1617373310678-e6f25a2d452a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80"
+                  alt=""
+                  class="img" /></picture
+            ></swiper-slide>
+            <swiper-slide>
+              <picture class="picture"
+                ><img
+                  src="https://images.unsplash.com/photo-1604785846291-2cd9192b1bef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2336&q=80"
+                  alt=""
+                  class="img" /></picture
+            ></swiper-slide>
+            <swiper-slide>
+              <picture class="picture"
+                ><img
+                  src="https://images.unsplash.com/photo-1550607326-2df38662b7dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+                  alt=""
+                  class="img" /></picture
+            ></swiper-slide>
+          </swiper>
+        </div>
   </section>
 </template>
 
 <script>
-export default {};
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Autoplay, Pagination, Navigation],
+    };
+  },
+};
 </script>
 
 <style scoped>
-.slider{
-    overflow: auto;
-    overflow-y: hidden;
+.slider {
+  overflow: auto;
+  overflow-y: hidden;
 }
 
-.grid {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: calc(70vw * 3);
+.swiper{
   overflow-x: scroll;
   align-content: center;
-  /* transform: translateX(-28.5%); */
-  gap: 0;
-}
-
-.slider::-webkit-scrollbar {
-  height: 0;
-  widows: 0;
-}
-
-.picture {
   height: 50rem;
-  background: red;
+  gap: 0;
   position: relative;
-
-  transition: .4s linear;
 }
-
-.picture:last-child{
-    z-index: -1;
+.swiper::after{
+  position: absolute;
+  content: '';
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #16161661;
+  z-index: 10;
 }
-
-.picture:last-child{
-    z-index: 10;
-}
-
-
 </style>
